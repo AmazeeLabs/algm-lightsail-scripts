31a3cf4468397976666ae367cf8b69244a520ae7
Bryan Gruneberg <hello@bryangruneberg.com>
2020-06-03 00:51:30 +0200

    Simple install

--- mailhog/install.sh
+++ mailhog/install.sh
@@ -14,9 +14,9 @@ SLACK_WEBHOOK=`cat /algm/SLACK_WEBHOOK`
 
 if [ ! -z "$SLACK_WEBHOOK" ]; then
   if [ ! -z "$MHPID" ]; then
-    /usr/bin/curl -X POST --data-urlencode "payload={\"channel\": \"#algm-mailhog\", \"username\": \"ALGM Lightsail\", \"text\": \"MailHog pid is $MHPID.\", \"icon_emoji\": \":pig:\"}" $SLACK_WEBHOOK
+    /usr/bin/curl -X POST --data-urlencode "payload={\"channel\": \"#algm-mailhog\", \"username\": \"ALGM Lightsail\", \"text\": \":pig: MailHog pid is $MHPID.\", \"icon_emoji\": \":pig:\"}" $SLACK_WEBHOOK
   else
-    /usr/bin/curl -X POST --data-urlencode "payload={\"channel\": \"#algm-mailhog\", \"username\": \"ALGM Lightsail\", \"text\": \"MailHog is not running.\", \"icon_emoji\": \":pig:\"}" $SLACK_WEBHOOK
+    /usr/bin/curl -X POST --data-urlencode "payload={\"channel\": \"#algm-mailhog\", \"username\": \"ALGM Lightsail\", \"text\": \":pig: MailHog is not running.\", \"icon_emoji\": \":pig:\"}" $SLACK_WEBHOOK
   fi
 else
   echo "There is no SLACK_WEBHOOK to notify ALGM" | logger
