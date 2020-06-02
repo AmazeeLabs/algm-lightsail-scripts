ab0fbf866ba8b5081e34bd7b90545e31cd40c356
Bryan Gruneberg <hello@bryangruneberg.com>
2020-06-03 00:25:27 +0200

    Simple install

--- mailhog/install.sh
+++ mailhog/install.sh
@@ -2,3 +2,6 @@ apt-get -y install golang-go
 mkdir -p /opt/go
 export GOPATH=/opt/go
 go get github.com/mailhog/MailHog
+cp $INSTALLPWD/mailhog.service /etc/systemd/system/mailhog.service
+sudo systemctl enable mailhog
+sudo systemctl start mailhog
