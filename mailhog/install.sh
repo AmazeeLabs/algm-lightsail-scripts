584f91a0edc4cf5681897bcd8be164880891a5a7
Bryan Gruneberg <hello@bryangruneberg.com>
2020-06-03 01:08:12 +0200

    Simple install

--- mailhog/install.sh
+++ mailhog/install.sh
@@ -8,16 +8,4 @@ sudo systemctl start mailhog
 
 cp $INSTALLPWD/cron/mailhog /etc/cron.daily/
 chmod +x /etc/cron.daily/mailhog
-
-MHPID=`pidof MailHog`
-SLACK_WEBHOOK=`cat /algm/SLACK_WEBHOOK`
-
-if [ ! -z "$SLACK_WEBHOOK" ]; then
-  if [ ! -z "$MHPID" ]; then
-    /usr/bin/curl -X POST --data-urlencode "payload={\"channel\": \"#algm-mailhog\", \"username\": \"ALGM Lightsail\", \"text\": \":pig: MailHog pid is $MHPID.\", \"icon_emoji\": \":pig:\"}" $SLACK_WEBHOOK
-  else
-    /usr/bin/curl -X POST --data-urlencode "payload={\"channel\": \"#algm-mailhog\", \"username\": \"ALGM Lightsail\", \"text\": \":pig: MailHog is not running.\", \"icon_emoji\": \":pig:\"}" $SLACK_WEBHOOK
-  fi
-else
-  echo "There is no SLACK_WEBHOOK to notify ALGM" | logger
-fi
+/etc/cron.daily/mailhog
