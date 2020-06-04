8ec142a360b548c2cb9ff73cae81ba06e7cef575
Bryan Gruneberg <hello@bryangruneberg.com>
2020-06-04 13:29:58 +0200

    Simple install

--- blue-value/install.sh
+++ blue-value/install.sh
@@ -1,3 +1,5 @@
 cp $INSTALLPWD/cron.d/blue-value /etc/cron.d/blue-value
 chmod +x $INSTALLPWD/deploy
+
 curl -L "https://github.com/amazeeio/lagoon-cli/releases/download/0.9.1/lagoon-cli-0.9.1-linux-amd64" -o /usr/local/bin/lagoon
+chmod +x /usr/local/bin/lagoon
